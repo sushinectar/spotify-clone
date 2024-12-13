68a595aa7bfb8479ac6219a60de1ab75e6d3b98f
sushinectar <lucianoxjktop@gmail.com>
2024-12-13 01:56:18 -0300

    first commit

---
+++ src/app/_layout.js
@@ -0,0 +1,9 @@
+import '../../global.css';
+
+import { Slot } from 'expo-router';
+
+export default function Layout() {
+  return (
+    <Slot />
+  )
+}
---
+++ src/app/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    <View className="flex m-auto">
+      <Text className="font-black text-yellow-500 text-xl">Hello World</Text>
+    </View>
+  );
+}
